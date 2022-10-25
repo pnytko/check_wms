9c264635d34b2d3c65c78aab1ddf58ec658138d9
pnytko <pnytko98@gmail.com>
2022-10-25 11:44:31 +0200

    mbadyl edit

--- check_wms.py
+++ check_wms.py
@@ -1,6 +1,6 @@
 from owslib.wms import WebMapService
 
-adres_wms = input(str("Podaj adres WMS: "))
+adres_wms = 'https://mapserver.bdl.lasy.gov.pl/ArcGIS/services/WMS_BDL_Mapa_turystyczna/MapServer/WMSServer'
 wms = WebMapService(adres_wms)
 
 title = wms.identification.title
@@ -11,6 +11,12 @@ abstract = wms.identification.abstract
 
 print(f'{title}\n{type}\n{version}\n{layer_names}\n')
 
+header = 'title;index\n'
+with open('output.csv', 'w') as output_file:
+    output_file.write(header)
+
+
+output_file = open('output.csv', 'a')
 for layer_instance in layer_names:
     layer_title = wms[layer_instance].title
     layer_query = wms[layer_instance].queryable
@@ -19,6 +25,7 @@ for layer_instance in layer_names:
     layer_bbox84 = wms[layer_instance].boundingBoxWGS84
     layer_crs = wms[layer_instance].crsOptions
     layer_styles = wms[layer_instance].styles
+    output_file.write(f'{layer_title};{layer_instance}\n')
     print(layer_title)
 
-print(layer_names)
+output_file.close()
